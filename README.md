77a22de07aa8ce500f618f13229f228880dce11a
SANTHOSH KANNA <praddossanthosh@gmail.com>
2022-12-30 00:19:31 +0530

    final commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjectBooksdisplayusingpagination
-To display the details of the book ., Using pagination the user will navigate from one page to another, in each page you will be able to get the details of the book .
--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 
-const baseurl='https://www.anapioficeandfire.com/api/books';
-const booknumber=1;
+const baseurl = 'https://www.anapioficeandfire.com/api/books';
+let booknumber=new URLSearchParams(window.location.search).get('id');
+console.log(booknumber);
+if(booknumber==null)
+booknumber=1;
+console.log(booknumber)
+let getLoader = document.getElementById("loading");
+const showLoading = () => {
+    getLoader.style.display = "block";
+    setInterval((hideLoading)=>{
+        getLoader.style.display="none";
+    },2400);
+}
 
-const fetchdatafromurl= async()=>{
-    try{
-    const response= await fetch(`${baseurl}/${booknumber}`);
-    const data= await response.json();
-    displaydata(data);
+const fetchdatafromurl = async () => {
+    showLoading();
+    try {
+        const response = await fetch(`${baseurl}/${booknumber}`);
+        const data = await response.json();
+        displaydata(data);
+
     }
-    catch(err){
+    catch (err) {
+        hideLoading();
         console.error(err);
         throw new Error(err);
     }
 
 }
-const displaycharacters=async(data,characternumber)=>{
-try{
-       let  response=await fetch(data.characters[characternumber]);
-        let character=await response.json();
-        return(character.name);
-}
-catch(err){
-    console.error(err);
-    throw new Error(err);
-}
+const displaycharacters = async (data, characternumber) => {
+    // showLoading();
+    try {
+        let response = await fetch(data.characters[characternumber]);
+        let character = await response.json();
+        // hideLoading();
+        return (character.name);
+
+    }
+    catch (err) {
+        console.error(err);
+        throw new Error(err);
+    }
 };
-const displaydata=async(data)=>{
-    try{
-    let bookdetails="";
-    bookdetails+=`
+const displaydata = async (data) => {
+    try {
+        let bookdetails = "";
+        bookdetails += `
     <div class="shadow border-1 rounded p-3 m-2 d-flex flex-column justify-content-start align-items-start container detailsbox" id="">
     <div class="m-2 fs-5 ">
        <b> <label for="">Name of the Book:</label></b>
@@ -57,20 +74,57 @@ const displaydata=async(data)=>{
     
     <div class="m-2 fs-5 ">
     <b><label>Important Characters in the book:</b>
-        <ol class="d-flex  flex-column align-items-center justify-content-start">
-            <li> ${await displaycharacters(data,0)}</li>
-            <li> ${await  displaycharacters(data,1)}</li>
-            <li> ${await  displaycharacters(data,2)}</li>
-            <li> ${await  displaycharacters(data,3)}</li>
-            <li> ${await  displaycharacters(data,4)}</li>
-        </ol>
+    <div class="table-responsive">
+    <table class="table table-sm">
+    <thead>
+    <tr>
+        <th>S.No</th>
+        <th>Name</th>
+    </tr>
+    </thead>
+        <tbody>
+    <tr>
+    <td>
+    1</td>
+    <td>
+    ${await displaycharacters(data, 23)}</td>
+    </tr>
+    <tr>
+    <td>
+    2</td>
+    <td>
+    ${await displaycharacters(data, 8)}</td>
+    </tr>
+    <tr>
+    <td>
+    3</td>
+    <td>
+    
+    ${await displaycharacters(data, 12)}</td>
+    </tr>
+    <tr>
+    <td>
+    4</td>
+    <td>
+    ${await displaycharacters(data, 17)}</td>
+    </tr>
+    <tr>
+    <td>
+    5</td>
+    <td>
+    ${await displaycharacters(data, 35)}</td>
+    </tr>
+    </tr>
+    </tbody>
+    </table>
+    </div>
+        
     </div>
 </div>`;
-
-document.getElementById("detailscontainer").innerHTML=bookdetails;
-displaycharacters(data);
+        
+        document.getElementById("detailscontainer").innerHTML = bookdetails;
     }
-    catch(err){
+    catch (err) {
         console.error(err);
         throw new Error(err);
     }
